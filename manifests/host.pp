f309e9dbe307ddadf48936bef204ef6c7874cf7e
Pete de Zwart <pdzwart@atlassian.com>
2012-06-22 18:21:41 +1000

    PERF-250: Class is isc-dhcp not isc-dhcpd.

--- manifests/host.pp
+++ manifests/host.pp
@@ -12,21 +12,21 @@
 #
 # == Examples
 #
-#    isc-dhcpd::host { 'bar.foo.com':
+#    isc-dhcp::host { 'bar.foo.com':
 #        MAC        => '0:0:c0:5d:bd:95',
 #        routers    => [ '192.168.0.1', '192.168.0.2' ],
 #    }
 #
-define isc-dhcpd::host($mac = undef,
+define isc-dhcp::host($mac = undef,
     $routers = undef) {
 
-    file { "$isc-dhcpd::dhcpd_conf_local_file_fragments_directory/01_dhcpd.conf.local_host_fragment_$name":
+    file { "$isc-dhcp::dhcp_conf_local_file_fragments_directory/01_dhcp.conf.local_host_fragment_$name":
         ensure  => file,
         owner   => root,
         group   => root,
         mode    => '0644',
-        content => template('isc-dhcpd/dhcpd.conf.local_host_fragment.erb'),
-        require => File[$isc-dhcpd::dhcpd_conf_local_file_fragments_directory],
-        notify  => Exec[$isc-dhcpd::dhcpd_conf_local_file_assemble],
+        content => template('isc-dhcp/dhcp.conf.local_host_fragment.erb'),
+        require => File[$isc-dhcp::dhcp_conf_local_file_fragments_directory],
+        notify  => Exec[$isc-dhcp::dhcp_conf_local_file_assemble],
     }
 }

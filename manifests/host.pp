c5f0a2dbedb622b71d66be68d12dc818faf97f9b
Pete de Zwart <pdzwart@atlassian.com>
2012-06-22 17:57:29 +1000

    PERF-250: Added in DHCP hosts as file parts for dhcpd.conf.local.

---
+++ manifests/host.pp
@@ -0,0 +1,32 @@
+# ISC DHCP Server- Host type
+#
+# == Parameters
+#
+# [*MAC*]
+#   Machine Address Code of the host.
+#
+# [*routers*]
+#   Array of optional routers.
+#
+# == Variables
+#
+# == Examples
+#
+#    isc-dhcpd::host { 'bar.foo.com':
+#        MAC        => '0:0:c0:5d:bd:95',
+#        routers    => [ '192.168.0.1', '192.168.0.2' ],
+#    }
+#
+define isc-dhcpd::zone($MAC = undef,
+    $routers = undef) {
+
+    file { "$isc-dhcpd::dhcpd_conf_local_file_fragments_directory/01_dhcpd.conf.local_host_fragment_$name":
+        ensure  => file,
+        owner   => root,
+        group   => root,
+        mode    => '0644',
+        content => template('isc-dhcpd/dhcpd.conf.local_host_fragment.erb'),
+        require => File[$isc-dhcpd::dhcpd_conf_local_file_fragments_directory],
+        notify  => Exec[$isc-dhcpd::dhcpd_conf_local_file_assemble],
+    }
+}
